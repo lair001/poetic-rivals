4f629a862eec5a370cee48c113eca5ebad938f03
Samuel Lair <lair001@gmail.com>
2016-11-09 04:21:39 -0500

    devise/omniauth setup

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,10 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+  def facebook
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    sign_in_and_redirect @user
+  end
+
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
   devise :omniauthable, :omniauth_providers => [:facebook]
   devise :registerable, :recoverable, :rememberable, :trackable, :validatable
 
+	def self.from_omniauth(auth)
+		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+			user.username = auth.info.email
+			user.email = auth.info.email
+			user.password = Devise.friendly_token[0,20]
+		end
+	end
+
 end
