729aa0d64afeaf8e65e223eafdd1a511deadcd80
Samuel Lair <lair001@gmail.com>
2016-11-11 05:33:24 -0500

    naming convention adjustment

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { sessions: 'users/sessions', passwords: 'users/passwords', registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
 
-    root 'visitor#home'
+    root 'visitor#index'
     get '/aqm' => 'visitor#aqm'
 end
