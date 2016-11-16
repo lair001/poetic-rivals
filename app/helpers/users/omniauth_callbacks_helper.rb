2f2eb5ca7b5816fe0b705ba962fe4410cf705cb1
Samuel Lair <lair001@gmail.com>
2016-11-15 22:25:54 -0500

    tweaking of flash messages

--- app/helpers/users/omniauth_callbacks_helper.rb
+++ app/helpers/users/omniauth_callbacks_helper.rb
@@ -4,11 +4,12 @@ module Users
 
 		def omniauth_authentication
 			@user = User.from_omniauth(request.env["omniauth.auth"])
-		    if @user
-		      sign_in_and_redirect(@user)
-		    else
-		      redirect_to new_user_registration_path
-		    end
+			if @user
+				sign_in_and_redirect(@user)
+			else
+				flash[:error] = "Account not found."
+				redirect_to new_user_registration_path
+			end
 		end
 
 	end
