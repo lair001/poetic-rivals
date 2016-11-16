406d066492da1ebe4dfed570e0afd4b5aa7e0132
Samuel Lair <lair001@gmail.com>
2016-11-16 00:42:26 -0500

    styling update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,8 +9,28 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-    configure_sign_up_params
-    super
+    configure_sign_up_params # need to manually configure params since authentication key is username instead of the default (email)
+
+    #begin code copied from Devise repo
+    build_resource(sign_up_params)
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      @current_path_name = "new_user_session" # need to manually set this for the title block to display
+      respond_with resource
+    end
   end
 
   # GET /resource/edit
