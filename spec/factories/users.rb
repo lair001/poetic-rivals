981be752e8c4ba5af909ef860230ef595f850a18
Samuel Lair <lair001@gmail.com>
2016-11-16 22:37:26 -0500

    made factory for users

---
+++ spec/factories/users.rb
@@ -0,0 +1,26 @@
+FactoryGirl.define do
+	factory :user do
+		sequence(:username) { |n| "lair00#{n}" }
+		sequence(:username) { |n| "lair00#{n}@example.com" }
+		password "password"
+		created_at { 10.days.ago }
+		updated_at { Time.now }
+	end
+
+	trait :banned do
+		role 'banned'
+	end
+
+	trait :admininistrator do
+		role 'admininistrator'
+	end
+
+	trait :moderator do
+		role 'moderator'
+	end
+
+	trait :superuser do
+		role 'superuser'
+	end
+
+end
