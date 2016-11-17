8ef3f1a989d239b941954e3efa24c0d6ea308d6c
Samuel Lair <lair001@gmail.com>
2016-11-17 03:12:44 -0500

    added tests for genres

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it 'knows its name' do
+		genre = fake_genre
+		expect(build(:genre, name: genre).name).to eq(genre)
+	end
+
+	it "isn't banned by default" do
+		expect(build(:genre).banned?).to eq(false)
+	end
+
+	it "knows if it has been banned" do
+		expect(build(:genre, banned?: true).banned?).to eq(true)
+	end
+
+	it 'has many poems' do
+		genre1 = create(:genre)
+		poem1 = create(:poem)
+		poem2 = create(:poem)
+		genre1.poems.push(poem1, poem2)
+		genre1.save
+		expect(genre1.poems).to include(poem1, poem2)
+	end
+
+
 end
