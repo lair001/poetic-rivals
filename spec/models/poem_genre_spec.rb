b5c0e98bf2d88dbc1e6c22a12f989849ca91e39e
Samuel Lair <lair001@gmail.com>
2016-11-17 02:52:19 -0500

    wrote tests for poem genre

--- spec/models/poem_genre_spec.rb
+++ spec/models/poem_genre_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe PoemGenre, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe PoemGenre, type: :model do  
+
+	it 'knows about its poem' do
+		poem = build(:poem)
+		expect(build(:poem_genre, poem: poem).poem).to eq(poem)
+	end
+
+	it 'knows about its genre' do
+		genre = build(:genre)
+		expect(build(:poem_genre, genre: genre).genre).to eq(genre)
+	end
+
 end
