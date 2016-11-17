a33a9e3a4e1bc74160390ab354e75ecfefaeaf29
Samuel Lair <lair001@gmail.com>
2016-11-16 22:45:55 -0500

    fixed factory for users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,22 +5,22 @@ FactoryGirl.define do
 		password "password"
 		created_at { 10.days.ago }
 		updated_at { Time.now }
-	end
 
-	trait :banned do
-		role 'banned'
-	end
+		trait :banned do
+			role 'banned'
+		end
 
-	trait :admininistrator do
-		role 'admininistrator'
-	end
+		trait :admininistrator do
+			role 'admininistrator'
+		end
 
-	trait :moderator do
-		role 'moderator'
-	end
+		trait :moderator do
+			role 'moderator'
+		end
 
-	trait :superuser do
-		role 'superuser'
-	end
+		trait :superuser do
+			role 'superuser'
+		end
 
+	end
 end
