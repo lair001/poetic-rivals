96853d0ea90df21f703c10116ba3b48bcb9d19c4
Samuel Lair <lair001@gmail.com>
2016-11-18 06:31:05 -0500

    configuring seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require_relative '../spec/rails_helper'
+
+build_stubbed(:poem)
