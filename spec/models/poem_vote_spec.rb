35ee9d0b03d34c108e5e1b4035e2d2aa3d9929f9
Samuel Lair <lair001@gmail.com>
2016-11-18 00:27:36 -0500

    implemented tests for poem_vote

--- spec/models/poem_vote_spec.rb
+++ spec/models/poem_vote_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe PoemVote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	it 'knows its poem' do
+		poem = build_stubbed(:poem)
+		expect(build_stubbed(:poem_vote, :up, poem: poem).poem).to eq(poem)
+	end
+
+	it 'knows its value' do
+		expect(build_stubbed(:poem_vote, :up).value).to eq(1)
+		expect(build_stubbed(:poem_vote, :down).value).to eq(-1)
+	end
+
+	it 'validates that it has a poem' do
+		expect(build(:poem_vote, :up, poem: nil).save).to eq(false)
+	end
+
+	it 'validates that it does not share its poem with another vote' do
+		poem_vote1 = create(:poem_vote, :up)
+		expect(build(:poem_vote, :up, poem: poem_vote1.poem).save).to eq(false)
+		expect(build(:poem_vote, :down, poem: poem_vote1.poem).save).to eq(false)
+	end
+
+	it 'validates that its value is either -1 or 1' do
+		expect(build(:poem_vote, :up).save).to eq(true)
+		expect(build(:poem_vote, :down).save).to eq(true)
+		expect(build(:poem_vote, value: rand(2..127)).save).to eq(false)
+		expect(build(:poem_vote, value: rand(-128..-2)).save).to eq(false)
+	end
+
 end
