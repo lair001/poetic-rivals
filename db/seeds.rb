fa6615c1458f400402c58901e7e8b6778809ce59
Samuel Lair <lair001@gmail.com>
2016-11-18 06:50:55 -0500

    configuring seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require_relative '../spec/rails_helper'
+include FactoryGirl::Syntax::Methods
+require_all 'spec/spec_helper/config/faker'
 
 build_stubbed(:poem)
