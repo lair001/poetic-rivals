dfbbd2d48c5636abe76f42c8d33769bea27fe0f7
Samuel Lair <lair001@gmail.com>
2016-11-19 04:02:06 -0500

    tweaking of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,31 @@
 Rails.application.routes.draw do
-  resources :commentaries
-  # devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	# devise_for :users
+	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  devise_for :users, controllers: { sessions: 'users/sessions', passwords: 'users/passwords', registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
+	devise_for :users, controllers: { sessions: 'users/sessions', passwords: 'users/passwords', registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
 
     root 'visitor#index'
     get '/aqm' => 'visitor#aqm'
 
 	resources :genres, only: [:new, :create, :edit, :update]
-	resources :poems, only: [:new, :create, :edit, :update, :destroy]
+	resources :commentaries, only: [:edit, :update, :destroy]
+	resources :poems, only: [:new, :create, :edit, :update, :destroy] do 
+		resources :voters, only: [:create, :destroy]
+		resources :commentaries, only: [:index, :new, :create]
+	end
 
 	resources :users, only: [:edit, :update, :show, :index] do
-		resources :poems, only: [:show, :index] do 
-			resources :commentaries, only: [:index, :new, :create]
-		end
-		resources :fan_idols, only: [:create]
-		resources :rival_victims, only: [:create]
+		resources :poems, only: [:show, :index]
+	end
+
+	resources :idols, only: [] do
+		resources :fans, only: [:create, :destroy]
+	end
+
+	resources :victims, only: [] do
+		resources :rivals, only: [:create, :destroy]
 	end
 
 	resources :commentaries, only: [:edit, :update, :destroy]
-	resources :fan_idols, only: [:destroy]
-	resources :rival_victims, only: [:destroy]
 
 end
