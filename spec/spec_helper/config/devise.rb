3804d85ea1e47ba87bd4f8bce9c1e51f770055de
Samuel Lair <lair001@gmail.com>
2016-11-19 10:34:02 -0500

    configured devise for tests

---
+++ spec/spec_helper/config/devise.rb
@@ -0,0 +1,23 @@
+module DeviseRequestSpecHelpers
+
+  include Warden::Test::Helpers
+
+  def sign_in(resource_or_scope, resource = nil)
+    resource ||= resource_or_scope
+    scope = Devise::Mapping.find_scope!(resource_or_scope)
+    login_as(resource, scope: scope)
+  end
+
+  def sign_out(resource_or_scope)
+    scope = Devise::Mapping.find_scope!(resource_or_scope)
+    logout(scope)
+  end
+
+end
+
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include DeviseRequestSpecHelpers, type: :request
+end
