3fbaef72e69a948cb8e98db8247ec61bdc37ebc3
Samuel Lair <lair001@gmail.com>
2016-11-20 08:12:19 -0500

    some more tests for RivalsController

--- app/controllers/rivals_controller.rb
+++ app/controllers/rivals_controller.rb
@@ -6,7 +6,7 @@ class RivalsController < ApplicationController
 		if @rival_victim.save
 			redirect_to previous_path_or_root
 		else
-			convert_model_errors_to_flash_error(model)
+			convert_model_errors_to_flash_error(@rival_victim)
 			redirect_to previous_path_or_root
 		end
 	end
--- spec/controllers/rivals_controller_spec.rb
+++ spec/controllers/rivals_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe RivalsController, type: :controller do
 
 	describe "#create" do
-		it 'creates a new RivalVictim if signed in as a user who is not banned' do
+		it 'creates a new RivalVictim if signed in as a user who is not banned and the resulting RivalVictim is successfully saved' do
 			save_models user_a, user_b
 			expect(RivalVictim.all.where(rival_id: user_a.id, victim_id: user_b.id).count).to eq(0)
 			sign_in user_a
@@ -11,12 +11,30 @@ RSpec.describe RivalsController, type: :controller do
 			expect(RivalVictim.all.where(rival_id: user_a.id, victim_id: user_b.id).first).to be_a(RivalVictim)
 		end
 
+		it 'does not create a new RivalVictim if signed in as a user who is not banned and the resulting RivalVictim is not successfully saved' do
+			save_models user
+			expect(RivalVictim.all.count).to eq(0)
+			sign_in user
+			begin
+				get :create, params: { victim_id: user.id + 1 }
+			rescue NameError
+			end
+			expect(RivalVictim.all.count).to eq(0)
+			begin
+				get :create, params: { victim_id: '' }
+			rescue NameError
+			end
+			expect(RivalVictim.all.count).to eq(0)
+			get :create, params: { victim_id: user.id }
+			expect(RivalVictim.all.count).to eq(0)
+		end
+
 		it 'does not create a new RivalVictim if signed in as a user who is banned' do
 			save_models banned, user
-			expect(RivalVictim.all.where(rival_id: banned.id, victim_id: user.id).count).to eq(0)
+			expect(RivalVictim.all.count).to eq(0)
 			sign_in banned
 			get :create, params: { victim_id: user.id }
-			expect(RivalVictim.all.where(rival_id: banned.id, victim_id: user.id).count).to eq(0)
+			expect(RivalVictim.all.count).to eq(0)
 		end
 	end
 
