5fc3bfe7a3ad906d4d2e0813bef65d44a2262a9e
Samuel Lair <lair001@gmail.com>
2016-11-23 05:27:23 -0500

    small corrections to mod/users_controller

--- app/controllers/mod/users_controller.rb
+++ app/controllers/mod/users_controller.rb
@@ -3,13 +3,15 @@ class Mod::UsersController < Mod::ApplicationController
 	def update
 		@user = User.find(params[:id])
 		authorize @user, :ban?
-		if @user.update(role: mod_user_params[:user][:role])
+		if @user.update(mod_user_params)
 			redirect_to previous_path_or_root
 		else
 			convert_model_errors_to_flash_error(@user)
 			redirect_to previous_path_or_root
 		end
 	end
+
+private
 
 	def mod_user_params
 		params.require(:user).permit(:role)
