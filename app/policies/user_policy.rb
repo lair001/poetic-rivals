60e663ca3d9a3134afa689f59a42cd31c0f4e22b
Samuel Lair <lair001@gmail.com>
2016-11-23 11:24:42 -0500

    started to implement user dashboard partial

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,7 +1,7 @@
 class UserPolicy < ApplicationPolicy
 
 	def ban?
-		if @user.mod? && (@record.normal? || @record.banned?)
+		if @user.moderator? && (@record.normal? || @record.banned?)
 			true
 		elsif @user.superuser? && !@record.superuser?
 			true
