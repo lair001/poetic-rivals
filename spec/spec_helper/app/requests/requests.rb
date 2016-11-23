e64f0b8c384ec31ad5e7e168cd2fb19723883723
Samuel Lair <lair001@gmail.com>
2016-11-23 13:34:21 -0500

    refactored some testing methods into a module: RequestsSpecHelper

--- spec/spec_helper/app/requests/requests.rb
+++ spec/spec_helper/app/requests/requests.rb
@@ -1,43 +1,44 @@
-def expect_redirect
-  expect(response.status).to eq(302)
-  follow_redirect!
-  expect(response.status).to eq(200)
-end
+module RequestsSpecHelper
 
-def expect_path(symbol, model = nil)
-	expect(path).to eq(self.send("#{symbol}_path"))
-	if self.respond_to?("#{symbol}_page_title")
-		if self.method("#{symbol}_page_title").arity != 0
-			expect(response.body).to include(self.send("#{symbol}_page_title", model))
-		else
-			expect(response.body).to include(self.send("#{symbol}_page_title"))
+	def expect_redirect
+	  expect(response.status).to eq(302)
+	  follow_redirect!
+	  expect(response.status).to eq(200)
+	end
+
+	def expect_path(symbol, model = nil)
+		expect(path).to eq(self.send("#{symbol}_path"))
+		if self.respond_to?("#{symbol}_page_title")
+			if self.method("#{symbol}_page_title").arity != 0
+				expect(response.body).to include(self.send("#{symbol}_page_title", model))
+			else
+				expect(response.body).to include(self.send("#{symbol}_page_title"))
+			end
+		end
+		if self.respond_to?("#{symbol}_page_tagline")
+			if self.method("#{symbol}_page_tagline").arity != 0
+				expect(response.body).to include(self.send("#{symbol}_page_tagline", model))
+			else
+				expect(response.body).to include(self.send("#{symbol}_page_tagline"))
+			end
 		end
 	end
-	if self.respond_to?("#{symbol}_page_tagline")
-		if self.method("#{symbol}_page_tagline").arity != 0
-			expect(response.body).to include(self.send("#{symbol}_page_tagline", model))
-		else
-			expect(response.body).to include(self.send("#{symbol}_page_tagline"))
-		end
+
+	def expect_redirect_to_previous_path_or_root
+		expected_path = previous_path_or_root
+		expect_redirect
+		expect(path).to eq(expected_path)
 	end
+
+	def expect_unauthorized_access_or_action
+		expect_redirect
+		expect_path(:root)
+		expect(response.body).to include(unauthorized_access_or_action_message)
+	end
+
+	alias_method :expect_unauthorized_action, :expect_unauthorized_access_or_action
+	alias_method :expect_unauthorized_access, :expect_unauthorized_access_or_action
+
 end
 
-def expect_redirect_to_previous_path_or_root
-	expected_path = previous_path_or_root
-	expect_redirect
-	expect(path).to eq(expected_path)
-end
-
-def expect_unauthorized_action
-	expect_unauthorized_access_or_action
-end
-
-def expect_unauthorized_access
-	expect_unauthorized_access_or_action
-end
-
-def expect_unauthorized_access_or_action
-	expect_redirect
-	expect_path(:root)
-	expect(response.body).to include(unauthorized_access_or_action_message)
-end
+include RequestsSpecHelper
