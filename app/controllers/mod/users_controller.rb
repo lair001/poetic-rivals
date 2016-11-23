8dbc6983a3c8391c87875343467cb17f0aa2dda5
Samuel Lair <lair001@gmail.com>
2016-11-23 14:28:23 -0500

    fixed mod users controller

--- app/controllers/mod/users_controller.rb
+++ app/controllers/mod/users_controller.rb
@@ -3,7 +3,8 @@ class Mod::UsersController < Mod::ApplicationController
 	def update
 		@user = User.find(params[:id])
 		authorize @user, :ban?
-		if @user.update(mod_user_params)
+		role = @user.banned? ? 1 : 0
+		if @user.update(role: role)
 			redirect_to previous_path_or_root
 		else
 			convert_model_errors_to_flash_error(@user)
