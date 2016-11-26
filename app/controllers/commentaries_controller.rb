590359f52ed87747b41ad23f8b0ef30ddd6554fc
Samuel Lair <lair001@gmail.com>
2016-11-26 01:26:45 -0500

    implemented CommentariesController#index

--- app/controllers/commentaries_controller.rb
+++ app/controllers/commentaries_controller.rb
@@ -1,2 +1,9 @@
 class CommentariesController < ApplicationController
+
+	def index
+		@poem = Poem.find(params[:poem_id])
+		authorize(@poem, :show)
+		render layout: 'application', locals: { model: @poem }
+	end
+
 end
