2c2a4376c3ffc9054e2743250b2acc6b75bb6f05
Samuel Lair <lair001@gmail.com>
2016-11-25 19:56:16 -0500

    fixed PoemsController#index

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,7 +1,12 @@
 class PoemsController < ApplicationController
 
 	def index
-		@poems = policy_scope(Poem.all)
+		if params.has_key?(:user_id)
+			@user = User.find(params[:user_id])
+			@poems = policy_scope(@user.poems)
+		else
+			@poems = policy_scope(Poem.all)
+		end
 	end
 
 	def show
