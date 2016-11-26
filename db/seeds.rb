7732d3ef8eabffbc0c0477b321f717e2d13bda5a
Samuel Lair <lair001@gmail.com>
2016-11-25 20:53:34 -0500

    added seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,65 @@
 include FactoryGirl::Syntax::Methods
 require_all 'spec/spec_helper/config/faker'
 
-build_stubbed(:poem)
+genres = []
+POETRY_GENRES.each do |poetry_genre|
+	create(:genre, name: poetry_genre)
+end
+
+users = []
+
+users << create(:user, :random, :superuser, username: 'lair001', email: 'lair001@gmail.com')
+users << create(:user, :random, :administrator, username: 'lair002', email: 'lair002@gmail.com')
+users << create(:user, :random, :moderator, username: 'lair003', email: 'lair003@gmail.com')
+
+2.times do
+	users << create(:user, :random, :banned)
+end
+
+15.times do
+	users << create(:user, :random)
+end
+
+poems = []
+200.times do
+	poems << create(:poem, author: users[rand(0..users.length-1)])
+end
+
+500.times do
+	begin
+		create(:poem_genre, poem: poems[rand(0..poems.length-1)])
+	rescue ActiveRecord::RecordInvalid
+	end
+end
+
+1000.times do
+	begin
+		create(:poem_voter, :up, poem: poems[rand(0..poems.length-1)])
+	rescue ActiveRecord::RecordInvalid
+	end
+end
+
+1000.times do
+	begin
+		create(:poem_voter, :down, poem: poems[rand(0..poems.length-1)])
+	rescue ActiveRecord::RecordInvalid
+	end
+end
+
+1000.times do
+	create(:commentary, poem: poems[rand(0..poems.length-1)], commentator: users[rand(0..users.length-1)])
+end
+
+1000.times do
+	begin
+		create(:fan_idol, fan_id: users[rand(0..users.length-1)], idol_id: users[rand(0..users.length-1)])
+	rescue ActiveRecord::RecordInvalid
+	end
+end
+
+1000.times do
+	begin
+		create(:rival_victim, rival_id: users[rand(0..users.length-1)], victim_id: users[rand(0..users.length-1)])
+	rescue ActiveRecord::RecordInvalid
+	end
+end
