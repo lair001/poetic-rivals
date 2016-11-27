e54f3049083a9411e04463882a2bb86b0cca6b15
Samuel Lair <lair001@gmail.com>
2016-11-27 11:32:17 -0500

    implementing commentaries controller

--- app/controllers/commentaries_controller.rb
+++ app/controllers/commentaries_controller.rb
@@ -6,4 +6,36 @@ class CommentariesController < ApplicationController
 		render layout: 'application', locals: { model: @poem }
 	end
 
+	def new
+		@commentary = Commentary.new
+	end
+
+	def create
+		@commentary = Commentary.new(commentary_params)
+		if @commentary.save
+			redirect_to user_poem_commentaries_path(@commentary.poem_author, @commentary.poem)
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@commentary = Commentary.find(params[:id])
+	end
+
+	def update
+		@commentary = Commentary.find(params[:id])
+		if @commentary.update(commentary_params)
+			redirect_to user_poem_commentaries_path(@commentary.poem_author, @commentary.poem)
+		else
+			render :edit
+		end
+	end
+
+private
+
+	def commentary_params
+		params.require(:commentary).permit(:comment, :commentator_id, :poem_id)
+	end
+
 end
