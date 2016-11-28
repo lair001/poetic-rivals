b36ca41270c8c39c3ce0c46f4295c360dee89e92
Samuel Lair <lair001@gmail.com>
2016-11-28 11:15:51 -0500

    implementing GenresHelper

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,12 +1,12 @@
 class GenresController < ApplicationController
 
 	def show
-		@genre = Genre.find(params[:id])
+		find_genre_by_params_id
 		authorize @genre
 	end
 
 	def index
-
+		sort_genres_by_name
 	end
 
 end
--- app/helpers/genres_helper.rb
+++ app/helpers/genres_helper.rb
@@ -1,2 +1,11 @@
 module GenresHelper
+
+	def find_genre_by_params_id
+		@genre = Genre.find(params[:id])
+	end
+
+	def sort_genres_by_name
+		@genres = Genre.ordered_by_ascending_name
+	end
+
 end
