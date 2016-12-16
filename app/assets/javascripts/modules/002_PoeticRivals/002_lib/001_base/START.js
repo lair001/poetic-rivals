94c1eed6989b3b95d4363aa5f7f2b2a9193298fd
Samuel Lair <lair001@gmail.com>
2016-12-16 03:41:36 -0500

    START.js now fails gracefully when #current_path_name is not present

--- app/assets/javascripts/modules/002_PoeticRivals/002_lib/001_base/START.js
+++ app/assets/javascripts/modules/002_PoeticRivals/002_lib/001_base/START.js
@@ -4,7 +4,9 @@
 
 		var pageFactory = modules.PoeticRivals.factories.page;
 
-		var currentPathName = document.getElementById('current_path_name').dataset.currentPathName;
+		var currentPathNameElement = document.getElementById('current_path_name');
+
+		var currentPathName = currentPathNameElement !== null ? currentPathNameElement.dataset.currentPathName : "";
 
 		if (currentPathName === 'aqm') {
 
