04d2b9ad09c1eff167b103b98afb3ebd8a1bc049
Samuel Lair <lair001@gmail.com>
2016-12-16 03:36:15 -0500

    adjustments to RoutesHelper::Mapper#generate_path_method_path_name_hash since teaspoon changed rake routes string

--- app/helpers/routes_helper/mapper.rb
+++ app/helpers/routes_helper/mapper.rb
@@ -23,9 +23,9 @@ module RoutesHelper
 		def generate_path_method_path_name_hash
 			path_method_path_name_hash = {}
 			routes_array.each do |route|
-				path = route[2].gsub("(.:format)", "")
+				path = route[2].gsub("(.:format)", "") if route[2].is_a?(String)
 				path_method_path_name_hash[path] = {} if !path_method_path_name_hash.has_key?(path)
-				route[1].split("|").each { |method| path_method_path_name_hash[path][method] = route[0] }
+				route[1].split("|").each { |method| path_method_path_name_hash[path][method] = route[0] } if route[1].is_a?(String)
 			end
 			path_method_path_name_hash
 		end
