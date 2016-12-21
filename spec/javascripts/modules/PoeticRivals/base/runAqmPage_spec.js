d354cc7583edf55a117f41bd9df002ac56db8df1
Samuel Lair <lair001@gmail.com>
2016-12-21 11:43:18 -0500

    added tests for base#runAqmPage

---
+++ spec/javascripts/modules/PoeticRivals/base/runAqmPage_spec.js
@@ -0,0 +1,33 @@
+describe('PoeticRivals.base#runAqmPage', function() {
+
+	var aqmGetQuoteButton, jQueryAqmGetQuoteButton;
+
+	var base = modules.PoeticRivals.base;
+	var pageFactory = modules.PoeticRivals.factories.page;
+
+	beforeEach(function() {
+		fixture.set('<button id="aqm_get_quote"></button>');
+		aqmGetQuoteButton = document.getElementById("aqm_get_quote");
+		jQueryAqmGetQuoteButton = $("#aqm_get_quote");
+	});
+
+	afterEach(function() {
+		jQueryAqmGetQuoteButton.off();
+	});
+
+	it('creates an Aqm page object', function() {
+		spy1 = sinon.spy(pageFactory, "Aqm");
+		base.runAqmPage();
+		expect(spy1).to.have.been.calledWithNew;
+		spy1.restore();
+	});
+
+	it('uses the Aqm page object and jQuery to attach a click event to #aqm_get_quote', function() {
+		spy1 = sinon.spy(window, '$');
+		base.runAqmPage();
+		expect(spy1).to.have.been.calledWith('#aqm_get_quote');
+		expect(!!$._data(aqmGetQuoteButton, "events").click).to.be.true;
+		spy1.restore();
+	});
+
+});
