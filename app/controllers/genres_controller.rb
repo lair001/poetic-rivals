057ddeb3b28369447bde9a1855cb231076699796
Samuel Lair <lair001@gmail.com>
2017-01-05 18:32:12 -0500

    genresCount now cooresponds to the number of genres viewable by the current user

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -5,10 +5,10 @@ class GenresController < ApplicationController
 			f.html do
 				find_genre_by_params_id
 				authorize @genre
-				render layout: 'application', locals: { model: @genre, page_data: { genres_count: Genre.count, sort_position: params[:sort_position] } }
+				render layout: 'application', locals: { model: @genre, page_data: { genres_count: policy_scope(Genre).count, sort_position: params[:sort_position] } }
 			end
 			f.json do
-				# when requesting JSON, params[:id] corresponds to sort_order
+				# when requesting JSON, params[:id] corresponds to sort_position
 				@genre = policy_scope(Genre.ordered_by_ascending_name).page(params[:id]).per(1).first
 				render json: @genre
 			end
