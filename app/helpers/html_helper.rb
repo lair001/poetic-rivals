9f720ae026e8a57a6713b8f1fc7a9238d32538c6
Samuel Lair <lair001@gmail.com>
2017-01-04 22:03:03 -0500

    renamed HtmlHelper #render_title and #render_tagline to #render_page_title and #render_page_tagline

--- app/helpers/html_helper.rb
+++ app/helpers/html_helper.rb
@@ -4,7 +4,7 @@ module HtmlHelper
 		string.gsub(/\t/, "&emsp;&emsp;").gsub(/\u2003/, "&emsp;").gsub(/\r\n/, "<br>").gsub(/[\f\n\r]/, "<br>").gsub(/\v/, "<br><br>")
 	end
 
-	def render_title(model = nil)
+	def render_page_title(model = nil)
 		if self.respond_to?("#{current_path_name}_page_title")
 			title = model && self.method("#{current_path_name}_page_title").arity != 0 ? self.send("#{current_path_name}_page_title", model) : self.send("#{current_path_name}_page_title")
 			"<h1 id='page_title'>#{title}</h1>"
@@ -13,7 +13,7 @@ module HtmlHelper
 		end
 	end
 
-	def render_tagline(model = nil)
+	def render_page_tagline(model = nil)
 		if self.respond_to?("#{current_path_name}_page_tagline")
 			tagline = model && self.method("#{current_path_name}_page_tagline").arity != 0 ? self.send("#{current_path_name}_page_tagline", model) : self.send("#{current_path_name}_page_tagline")
 			"<h2 id='page_tagline'><em>#{tagline}</em></h2>"
