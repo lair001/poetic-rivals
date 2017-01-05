fb5c6d9153d255b9855ce7a436a28f594ea07b3b
Samuel Lair <lair001@gmail.com>
2017-01-05 18:28:40 -0500

    refactored GenresController#show so that the :id param corresponds to sort_position when requesting JSON

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,11 +1,17 @@
 class GenresController < ApplicationController
 
 	def show
-		find_genre_by_params_id
-		authorize @genre
 		respond_to do |f|
-			f.html { render layout: 'application', locals: { model: @genre, page_data: { genres_count: Genre.count, sort_position: params[:sort_position] } } }
-			f.json { render json: @genre }
+			f.html do
+				find_genre_by_params_id
+				authorize @genre
+				render layout: 'application', locals: { model: @genre, page_data: { genres_count: Genre.count, sort_position: params[:sort_position] } }
+			end
+			f.json do
+				# when requesting JSON, params[:id] corresponds to sort_order
+				@genre = policy_scope(Genre.ordered_by_ascending_name).page(params[:id]).per(1).first
+				render json: @genre
+			end
 		end
 	end
 
