895304665224970264b32fc794fabdfb07cfeff3
Samuel Lair <lair001@gmail.com>
2017-01-06 03:29:03 -0500

    implemented PoeticRivals.factories.page.Genre#onPreviousModelClick, #onPreviousModelClickError, #onNextModelClick, #onNextModelClickError

--- app/assets/javascripts/modules/002_lib/002_PoeticRivals/002_lib/001_factories/002_page/003_Genre.js
+++ app/assets/javascripts/modules/002_lib/002_PoeticRivals/002_lib/001_factories/002_page/003_Genre.js
@@ -32,6 +32,9 @@
 		var onGetGenreJSON = function(genreJSON) {
 			page.showableGenre = new viewModelFactory.ShowableGenre(genreJSON);
 			page.showableGenre.render();
+			if (page.sortPosition > page.genresCount) {
+				page.genresCount = page.sortPosition;
+			}
 		};
 
 		var setEventListeners = function() {
@@ -41,17 +44,70 @@
 
 		var onPreviousModelClick = debounce(function() {
 
+			if (page.genresCount >= 2) {
+
+				if (page.sortPosition === 1) {
+					page.sortPosition = page.genresCount;
+				} else {
+					page.sortPosition--;
+				}
+
+				page.getGenreJSON(page.onPreviousModelClickError);
+
+			}
+
 		}, 500, true);
+
+		var onPreviousModelClickError = function(errorJSON) {
+
+			console.log(errorJSON);
+			var recordNotFound = false;
+			errorJSON.responseJSON.errors.forEach(function(error) {
+				if (error.status === 404) {
+					recordNotFound = true;
+				}
+			});
+
+			if (recordNotFound && page.sortPosition >= 2) {
+				page.sortPosition--;
+				page.getGenreJSON(page.onPreviousModelClickError);
+			}
+
+		}
 
 		var onNextModelClick = debounce(function() {
 
+			if (page.genresCount >= 2) {
+
+				page.sortPosition++;
+				page.getGenreJSON(page.onNextModelClickError);
+
+			}
+
 		}, 500, true);
+
+		var onNextModelClickError = function(errorJSON) {
+			console.log(errorJSON);
+			var recordNotFound = false;
+			errorJSON.responseJSON.errors.forEach(function(error) {
+				if (error.status === 404) {
+					recordNotFound = true;
+				}
+			});
+
+			if (recordNotFound && page.sortPosition >= 2) {
+				page.sortPosition = 1;
+				page.getGenreJSON(page.onNextModelClickError);
+			}
+		}
 
 		page.getGenreJSON = getGenreJSON;
 		page.onGetGenreJSON = onGetGenreJSON;
 		page.setEventListeners = setEventListeners;
 		page.onPreviousModelClick = onPreviousModelClick;
+		page.onPreviousModelClickError = onPreviousModelClickError;
 		page.onNextModelClick = onNextModelClick;
+		page.onNextModelClickError = onNextModelClickError;
 
 	}
 
