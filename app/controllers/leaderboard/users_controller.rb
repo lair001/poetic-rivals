691e20891079bbab9789202e7f40b73dddfb84dd
Samuel Lair <lair001@gmail.com>
2017-01-09 00:09:29 -0500

    modified Leaderboard::UsersController#show to render an initial set of excluded ids as page data when rendering an html page

--- app/controllers/leaderboard/users_controller.rb
+++ app/controllers/leaderboard/users_controller.rb
@@ -4,7 +4,10 @@ class Leaderboard::UsersController < Leaderboard::ApplicationController
 		params[:excluded_ids] ? excluded_ids = params[:excluded_ids].split(',') : excluded_ids = []
 		@users = User.ordered_by_descending_score.page(1).where.not(id: excluded_ids)
 		respond_to do |f|
-			f.html {}
+			f.html do
+				excluded_ids = @users.collect{ |user| user.id.to_s }.join(',')
+				render layout: 'application', locals: { page_data: { excluded_ids: excluded_ids } }
+			end
 			f.json { render json: @users, fields: [:id, :username, :role, :created_at_date, :created_at_time, :score, :rounded_score_per_poem] }
 		end
 	end
