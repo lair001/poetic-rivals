579c703ba552667a7243251b8105e52c8e5a7bd0
Samuel Lair <lair001@gmail.com>
2017-01-09 04:05:33 -0500

    implemented page.GenreAuthors

---
+++ app/assets/javascripts/modules/002_lib/002_PoeticRivals/002_lib/001_factories/002_page/008_GenreAuthors.js
@@ -0,0 +1,34 @@
+(function() {
+
+	"use strict";
+
+	var pageFactory = modules.PoeticRivals.factories.page,
+		viewModelFactory = modules.PoeticRivals.factories.viewModel,
+		utils = modules.Utils,
+		debounce = utils.debounce;
+
+	pageFactory.GenreAuthors = function() {
+
+		var page = this;
+
+		pageFactory.FromPageData.call(page);
+
+		var setEventListeners = function() {
+			var scrollableUserIndexPage = new pageFactory.ScrollableIndex(page.indexId, page.jqTemplateClassName, page.apiUrl, page.excludedIds, page.newModelCallback);
+			scrollableUserIndexPage.setEventListeners();
+		}
+
+		var newModelCallback = function(modelJSON, indexId, jqTemplate) {
+			var indexableUser = new viewModelFactory.IndexableUser(modelJSON, indexId, jqTemplate);
+			return indexableUser;
+		}
+
+		page.setEventListeners = setEventListeners;
+		page.indexId = 'users_index';
+		page.jqTemplateClassName = 'indexable-user-template';
+		page.apiUrl = '/genres/' + page.genreId + '/authors';
+		page.newModelCallback = newModelCallback;
+
+	}
+
+})();
