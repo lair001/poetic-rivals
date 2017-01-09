e3af63d510bb5393c8884f4adf67a22f78bda03d
Samuel Lair <lair001@gmail.com>
2017-01-08 21:55:33 -0500

    modified Leaderboard::UsersController#index for json-based api

--- app/controllers/leaderboard/users_controller.rb
+++ app/controllers/leaderboard/users_controller.rb
@@ -1,7 +1,12 @@
 class Leaderboard::UsersController < Leaderboard::ApplicationController
 
 	def index
-		sort_users_by_score
+		params[:excluded_ids] ? excluded_ids = params[:excluded_ids].split(',') : excluded_ids = []
+		@users = User.ordered_by_descending_score.page(1).where.not(id: excluded_ids)
+		respond_to do |f|
+			f.html {}
+			f.json { render json: @users, fields: [:id, :username, :role, :created_at_date, :created_at_time, :score, :rounded_score_per_poem] }
+		end
 	end
 
 end
