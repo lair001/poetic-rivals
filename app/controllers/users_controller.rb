1f49d8da4d7a6430673ee29af83f3fb62bfa8c59
Samuel Lair <lair001@gmail.com>
2017-01-09 03:17:31 -0500

    modified UsersController#index to respond to json requests when on the genre authors path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,16 @@ class UsersController < ApplicationController
 		if current_page?(users_path)
 			sort_users_by_username_and_paginate
 		elsif current_page?(genre_authors_path)
-			if params.has_key?(:genre_id)
-				@genre = Genre.find(params[:genre_id])
-				sort_genre_authors_by_score
-				render layout: 'application', locals: { model: @genre }
-			else
-				@users = User.all
+			@genre = Genre.find(params[:genre_id])
+			authorize @genre, :show?
+			params[:excluded_ids] ? excluded_ids = params[:excluded_ids].split(',') : excluded_ids = []
+			@users = @genre.authors_ordered_by_descending_score.page(1).where.not(id: excluded_ids)
+			respond_to do |f|
+				f.html do
+					excluded_ids = @users.collect{ |user| user.id.to_s }.join(',')
+					render layout: 'application', locals: { model: @genre, page_data: { excluded_ids: excluded_ids } }
+				end
+				f.json { @users.count > 0 ? render(json: @users, fields: [:id, :username, :role, :created_at_date, :created_at_time, :score, :rounded_score_per_poem]) : render_record_not_found_json }
 			end
 		end
 	end
