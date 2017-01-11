21dcac0f44e930f0d3adc7471fe7dc6e5da9e061
Samuel Lair <lair001@gmail.com>
2017-01-10 19:13:54 -0500

    implemented Page.ClickableIndex

---
+++ app/assets/javascripts/modules/002_lib/002_PoeticRivals/002_lib/001_factories/002_page/005_ClickableIndex.js
@@ -0,0 +1,49 @@
+(function() {
+
+	"use strict";
+
+	var pageFactory = modules.PoeticRivals.factories.page,
+		jqWindow = $(window),
+		jqBody = $('.body'),
+		utils = modules.Utils,
+		debounce = utils.debounce;
+
+	pageFactory.ClickableIndex = function(indexId, jqTemplateClassName, apiUrl, excludedIds, newModelCallback, clickId, afterModelRenderCallback) {
+
+		var page = this,
+			jqClickElement = $('#' + clickId);
+
+		pageFactory.Index.call(page, indexId, jqTemplateClassName, apiUrl, excludedIds, newModelCallback);
+
+		var setEventListeners = function() {
+			if (jqWindow.height() > jqBody.height()) {
+				page.getModelsJSON(page.onClickError);
+			}
+			jqClickElement.on("click", page.onClick);
+		};
+
+		var onClick = debounce(function() {
+			page.getModelsJSON(page.onClickError);
+		}, 500, true);
+
+		var onClickError = function(errorJqXHR) {
+			page.onError(errorJqXHR, page.onNextRecordsNotFound, page.onRecordNotProcessed);
+		};
+
+		var onNextRecordsNotFound = function(error, errorJqXHR, self, recordNotProcessedCallback) {
+			jqClickElement.off("click", page.onClick);
+		};
+
+		if (typeof(afterModelRenderCallback) === "function") {
+			page.afterModelRender = afterModelRenderCallback;
+		}
+
+		page.clickId = clickId;
+		page.setEventListeners = setEventListeners;
+		page.onClick = onClick;
+		page.onClickError = onScrollError;
+		page.onNextRecordsNotFound = onNextRecordsNotFound;
+
+	}
+
+})();
