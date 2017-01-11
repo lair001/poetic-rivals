941357353d5768580334f40eec28c71f9a6f703f
Samuel Lair <lair001@gmail.com>
2017-01-11 02:15:57 -0500

    started to implement page.SubmitCommentaryForm

---
+++ app/assets/javascripts/modules/002_lib/002_PoeticRivals/002_lib/001_factories/002_page/012_SubmitCommentaryForm.js
@@ -0,0 +1,62 @@
+(function() {
+
+	"use strict";
+
+	var pageFactory = modules.PoeticRivals.factories.page,
+		jqWindow = $(window),
+		jqBody = $('.body'),
+		utils = modules.Utils,
+		debounce = utils.debounce;
+
+	pageFactory.SubmitCommentaryForm = function(indexId, jqTemplateClassName, apiUrl, newModelCallback, formId, afterModelRenderCallback) {
+
+		var page = this,
+			jqForm = $('#' + formId);
+
+		pageFactory.Index.call(page, indexId, jqTemplateClassName, apiUrl, newModelCallback);
+
+		var setEventListeners = function() {
+			jqForm.on("submit", page.onSubmit);
+		};
+
+		var jqTemplate = once(function() {
+			return $($("." + page.jqTemplateClassName)[0]).clone();
+		});
+
+		var onSubmit = debounce(function(event) {
+			event.preventDefault();
+			var data = $(this).serialize();
+			$.ajax(
+				{
+					url: page.apiUrl,
+					method: "POST",
+					data: data,
+					success: page.onPost,
+					error: page.onError
+				}
+			);
+		}, 5000, true);
+
+		var onPost = function(JSON) {
+			console.log(JSON);
+		};
+
+		var onError = function(jqXHR) {
+			console.log(jqXHR);
+			console.log(jqXHR.responseJSON);
+		}
+
+		page.indexId = indexId;
+		page.jqTemplateClassName = jqTemplateClassName;
+		page.apiUrl = apiUrl;
+		page.newModelCallback = newModelCallback;
+		page.formId = formId;
+		page.afterModelRenderCallback = afterModelRenderCallback;
+		page.setEventListeners = setEventListeners;
+		page.jqTemplate = jqTemplate;
+		page.onPost = onPost;
+		page.onError = onError;
+
+	}
+
+})();
