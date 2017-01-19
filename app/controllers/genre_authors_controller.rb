1af9f76c540a20d0609b8ec66ddbf88b95c2106b
Samuel Lair <lair001@gmail.com>
2017-01-18 20:00:50 -0500

    refactored GenreAuthorsController to use limit instead of page

--- app/controllers/genre_authors_controller.rb
+++ app/controllers/genre_authors_controller.rb
@@ -4,7 +4,7 @@ class GenreAuthorsController < ApplicationController
 		@genre = Genre.find(params[:genre_id])
 		authorize @genre, :show?
 		params[:excluded_ids] ? excluded_ids = params[:excluded_ids].split(',') : excluded_ids = []
-		@users = @genre.authors_ordered_by_descending_score.page(1).where.not(id: excluded_ids)
+		@users = @genre.authors_ordered_by_descending_score.where.not(id: excluded_ids).limit(Kaminari.config.default_per_page)
 		respond_to do |f|
 			f.html do
 				excluded_ids = @users.collect{ |user| user.id.to_s }.join(',')
